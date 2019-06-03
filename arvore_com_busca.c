4891f6891b1cd455384299ed6d3253060e25adb3
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2019-06-03 10:46:35 -0300

    UP

--- arvore_com_busca.c
+++ arvore_com_busca.c
@@ -8,16 +8,18 @@
 #include <string.h>
 
 #define tamanho 100
+#define buscando 20
 
 struct tnode * new_tnode(int v);
-void tprint(struct tnode *r);
-void tinsert(struct tnode **R, struct tnode *n);
-struct tnode* search(struct tnode *R, int v);
+void exibir(struct tnode *r);
+void iserir(struct tnode **R, struct tnode *n);
+struct tnode* pesquisa(struct tnode *R, int v);
 void rd(struct tnode * x);
 int tamanhoArvore(struct tnode *r);
 void preencherArvore(struct tnode **R);
 int max(int a, int b);
-int min(int a, int b);
+void exibirPesquisa(struct tnode *r);
+void exibirTamArvore(struct tnode *r);
 
 struct tnode{
 	int v;
@@ -33,10 +35,11 @@ int main(int argc, char **argv){
 	R->l = new_tnode(2);
 	R->r = new_tnode(6);
 	(R->l)->l = new_tnode(1);*/
-	//tinsert(&R, new_tnode(19));
+	//iserir(&R, new_tnode(19));
 	preencherArvore(&R);
-	printf("Numero de nos: %d\n\n", tamanhoArvore(R));
-	tprint(R);	
+	exibirTamArvore(R);
+	exibir(R);
+	exibirPesquisa(R);	
 	return 0;
 }
 
@@ -48,49 +51,49 @@ struct tnode * new_tnode(int v){
 		return w;
 }
 
-void tinsert(struct tnode **R, struct tnode *n){
+void iserir(struct tnode **R, struct tnode *n){
 	if(*R == NULL)
 		*R = n;
 	else{
 		if(n->v < (*R)->v)
-			tinsert(&((*R)->l), n);
+			iserir(&((*R)->l), n);
 		else
-			tinsert(&((*R)->l), n);
+			iserir(&((*R)->l), n);
 	}
 }
 
-struct tnode* search(struct tnode *R, int v){
+struct tnode* pesquisa(struct tnode *R, int v){
 	if(R != NULL){
 		if(R->v == v)
 		 return R;
-		if(R->v > v)
-			return search(R->l, v);
-		return search(R->r, v); 
+		if(R->v < v)
+			return pesquisa(R->l, v);
+		return pesquisa(R->r, v); 
 	}
 	return NULL;
 }
 
 /*void avl(struct tnode *R, int w){
 	v = w.p;
-	while(v != ){
-		
+	while(v != NULL){
+		v.h = max(h(v.l), h(v.r)) + 1		
 	}
 }
 
 void rd(struct tnode * x){
-	struct tnode *y = x->e;
-	struct tnode *c = y->d;
-	x->e = c;
-	y->d = x;
-	y->p = x->p;
-	x-> = y;
+	struct tnode *y = x->l;
+	struct tnode *c = y->r;
+	x->l = c;
+	y->r = x;
+	y->v = x->v;
+	x->v = y->v;
 	if(c != NULL)
-		c->p = x;
-	if(y-> != NULL){
-		if(y->pv > x->v)
-			y->p->c = y;
+		c->v = x->v;
+	if(y->v != NULL){
+		if(y->v->v > x->v)
+			y->v->c = y;
 		else
-			y->p->d = y;
+			y->v->d = y;
 	}
 }*/
 
@@ -100,17 +103,17 @@ int tamanhoArvore(struct tnode *r){
 	return 0; 
 }
 
-void tprint(struct tnode *r){
+void exibir(struct tnode *r){
 	if(r != NULL){
-		tprint(r->r);
+		exibir(r->r);
 		printf("%d ", r->v);
-		tprint(r->l);
+		exibir(r->l);
 	}
 }
 
 void preencherArvore(struct tnode **R){
 	for(int i = 1; i <= tamanho; i++)
-		tinsert(R, new_tnode(i));
+		iserir(R, new_tnode(i));
 }
 
 int max(int a, int b){
@@ -119,8 +122,14 @@ int max(int a, int b){
 	return b;
 }
 
-int min(int a, int b){
-	if(a < b)
-		return a;
-	return b;
+void exibirPesquisa(struct tnode *r){
+	if ((pesquisa(r, buscando))!= NULL){
+		printf("\n\nValor '%d' encontrado!", buscando);
+	}else{
+		printf("\n\nValor '%d' NAO encontrado!", buscando);
+	}
+}
+
+void exibirTamArvore(struct tnode *r){
+	printf("Numero de nos: %d\n\n", tamanhoArvore(r));
 }
